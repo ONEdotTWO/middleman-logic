04ab145c6a39299521a171962355012629e3a79d
Nathaniel Freeman <volmasoft@gmail.com>
2015-10-24 21:23:00 +0100

    updates

--- routes/logic.js
+++ routes/logic.js
@@ -2,8 +2,21 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.json({ message: "hello world!"});
+router.get('/', function (req, res) {
+
+    // Load configuration
+    var configuration = JSON.parse(
+        fs.readFileSync(configFile)
+    );
+
+    if(configuration.mode == "middleman")
+    {
+        // Get message
+        // If message contains # at start then take first token as 'to' number
+        // Send to number using sender
+        // If message doesn't contain # at start
+        // Send to middleman number
+    }
 });
 
 module.exports = router;
